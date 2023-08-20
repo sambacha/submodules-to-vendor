e746097acb506815f57413508a2cd6b0b73fe205
sam bacha <sam@manifoldfinance.com>
2023-08-20 01:11:58 -0700

    fix(shellcheck): SC2292

--- src/run.sh
+++ src/run.sh
@@ -1,16 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Output filename
-output_file="subtree_commands_$(date +%s).txt"
+# simplifies both the tag name used when making a release
+# and the computed version number take the date/time from the current
+# commit, and then append the hash.  That way the version number always
+# corresponds to a commit.
+VERSION_ID=${VERSION_ID:-$(git show -s "--format=%cd-%h" "--date=format:%Y%m%d-%H%M%S")}
+
+
+# @stdout Output filename
+output_file="subtree_commands_$VERSION_ID.txt"
+
 
 # Ensure .gitmodules file exists
-if [ ! -f .gitmodules ]; then
+if [[ ! -f .gitmodules ]]; then
     echo "⛔️ Error: .gitmodules file not found."
     exit 1
 fi
 
 # Clear or create the output file
-> "$output_file"
+: > "$output_file"
 
 # Parse the .gitmodules file and generate subtree commands
 while IFS= read -r line; do
@@ -25,7 +33,7 @@ while IFS= read -r line; do
         hash=$(git submodule status --cached "$path" | awk '{print $1}')
         
         # If hash is empty, raise an error
-        if [ -z "$hash" ]; then
+        if [[ -z "$hash" ]]; then
             echo "⛔️ Error: Failed to retrieve hash for submodule $name at path $path."
             exit 1
         fi
@@ -40,5 +48,7 @@ if grep -qv "^git subtree" "$output_file"; then
     echo "⛔️ Error: Output file contains invalid lines."
     exit 1
 fi
+sleep 1
 
 echo "Commands written to $output_file"
+exit 0
